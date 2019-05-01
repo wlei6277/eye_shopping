0806caea03acc78013cdedaa934eb4712897a139
Billy Leitch <wlei6277@gmail.com>
2019-05-01 12:15:33 +1000

    added seeding data for favourites and followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,6 @@ class User < ApplicationRecord
     has_many :favourites
     has_many :comments
     has_many :products
-<<<<<<< HEAD
     has_many :followers, foreign_key: "follower_id" 
     has_many :followings, foreign_key: "following_id"
-=======
-    has_many :followers,:class_name => "User" 
-    has_many :followings,:class_name => "User" 
->>>>>>> master
-
 end
