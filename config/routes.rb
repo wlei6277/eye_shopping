78f0536d7bfa0df51836cf0248fad27dd5dbbbd7
Dodam <lapeconera@gmail.com>
2019-05-02 15:49:15 +1000

    added mypage controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path: "/", path_names: {sign_in: "login"}
   get "/", to: "pages#home", as: "root"
+  get "/myboard", to: "pages#myboard", as: "myboard"
+  get "/myaccount", to: "pages#myaccount", as: "myaccount"
+  
   resources :comments
   resources :products
   resources :followings
