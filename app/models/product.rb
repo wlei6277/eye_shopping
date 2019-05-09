4daabe1da2d9de324327ada28d1b7e0c32b371be
Billy Leitch <wlei6277@gmail.com>
2019-05-09 15:31:59 +1000

    Fixed problem where you couldn't view a users board without being signed in. Set a default image for products and users if none is provided by the user. Put validation on the product form so url link must now be provided.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,17 @@
 class Product < ApplicationRecord
+  after_initialize :assign_default_values
+  validates :name, :url_link, presence: true
+
   belongs_to :user
   belongs_to :department
+
+  #get deleted if the product is removed
   has_many :comments, dependent: :destroy
-  #get deleted if the post is removed
   has_many :favourites, dependent: :destroy
+  
   enum color: { red: 0, blue: 1, green: 2, yellow: 3, brown: 4, black: 5 }
   has_one_attached :picture
+
   #validation method to check if a user has favourited a product or not
   #this takes two arguments:
   #  1. The id of the user which is logged in
@@ -13,6 +19,16 @@ class Product < ApplicationRecord
   #if a query on the Favourite table, using the user and product ids, returns an array greater than 0 then the product has already been favourited  
   def user_favourites(current_user, product)
     Favourite.where(user_id: current_user, product_id: product) > 0
+  end
+
+  def assign_default_values
+    unless self.picture.attached?
+      p "second statement"
+      self.picture.attach(io: File.open(
+      Rails.root.join('app', 'assets', 'images', 'no-product-picture.png')),
+      filename: 'no-product-picture.png', content_type: 'image/png'
+      )
+    end
   end
 
 
