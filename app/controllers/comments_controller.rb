cfa4bc596d86341b5a47e5920b47d19ae31a2cc9
Billy Leitch <wlei6277@gmail.com>
2019-05-09 12:33:34 +1000

    Fixed the ability for user to edit name and profile picture. Fixed comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
-  before_action :set_product, only: [:show, :edit, :update, :destroy, :create]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /comments
   # GET /comments.json
@@ -35,7 +35,7 @@ class CommentsController < ApplicationController
     @comment = Comment.create(comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @product, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.product, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
