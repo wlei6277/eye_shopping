4d0f630978d4e828f4d2aef492066f6f6b6eb374
Billy Leitch <wlei6277@gmail.com>
2019-05-10 13:51:53 +1000

    added skeleton for test cases

---
+++ test/cypress/integration/test.js
@@ -0,0 +1,40 @@
+describe('Rails Eye Shopper test', function() {
+    // Test 1: Can a user log-in?
+        it("Log-in user", function() {
+        });
+    // Test 2: Can a user log-out?
+        it("Log-out user", function() {
+        });
+    // Test 3: Can a user create a product?
+        it("Create product", function() {
+        });
+    // Test 4: Can a user edit a product?
+        it("Edit product", function() {
+        });
+    // Test 5: Can a user delete a product?
+        it("Delete product", function() {
+        });
+    // Test 6: Can a user follow another user?
+        it("Follow user", function()) {
+
+        });
+    // Test 7: Can a user unfollow another user?
+        it("Un-follow user", function()) {
+
+        });
+    // Test 8: Can a user favourite a product?
+        it("Favourite product", function()) {
+
+        });
+
+    // Test 9: Can a user unfavourite a product?
+        it("Un-favourite product", function()) {
+
+        });
+
+    // Test 10: Can a user access the my-board page?
+        it("Access my-board", function()) {
+
+        });
+
+});
