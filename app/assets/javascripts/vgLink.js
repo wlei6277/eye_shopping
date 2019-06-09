8ff158860a79abc1e585a8ea19c6cddaa20bb01b
Billy Leitch <wlei6277@gmail.com>
2019-06-09 15:53:45 +1000

    adding some stuff to see if this works

--- app/assets/javascripts/vgLink.js
+++ app/assets/javascripts/vgLink.js
@@ -1,9 +1,12 @@
-var vglnk = {key: `${Rails.application.credentials.dig(:vglink)}`};
-(function(d, t) {
-  var s = d.createElement(t);
-    s.type = 'text/javascript';
-    s.async = true;
-    s.src = '//cdn.viglink.com/api/vglnk.js';
-  var r = d.getElementsByTagName(t)[0];
-    r.parentNode.insertBefore(s, r);
-}(document, 'script'));
+function vgLink (key) {
+  var vglnk = {key: key};
+  (function(d, t) {
+    var s = d.createElement(t);
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = '//cdn.viglink.com/api/vglnk.js';
+    var r = d.getElementsByTagName(t)[0];
+      r.parentNode.insertBefore(s, r);
+  }(document, 'script'));
+ 
+};
